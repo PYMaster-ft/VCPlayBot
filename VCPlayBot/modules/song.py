6d3e75ec3966180866e515654bb850fda7357bec
chamindu pushpika <72345334+PYMaster-ft@users.noreply.github.com>
2021-10-05 23:24:19 +0530

    Update song.py

--- VCPlayBot/modules/song.py
+++ VCPlayBot/modules/song.py
@@ -35,7 +35,7 @@ def song(client, message):
     for i in message.command[1:]:
         query += " " + str(i)
     print(query)
-    m = message.reply("🔎 Finding the song...")
+    m = message.reply("🔎 Finding the song.mp3..")
     ydl_opts = {"format": "bestaudio/best"}
     try:
         results = YoutubeSearch(query, max_results=1).to_dict()
